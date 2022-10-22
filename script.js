b6e57f7056f5c46746e5460d616616cdd3160af8
MarKraken <110046283+mariasmoUN@users.noreply.github.com>
2022-10-22 17:39:24 -0500

    Cambiando archivos

---
+++ script.js
@@ -0,0 +1,20 @@
+const btn = document.getElementById('button');
+
+/* Barra de navegacion */
+
+/* Boton Menu */
+btn.addEventListener('click', function() {
+    if (this.classList.contains('active')) {
+        this.classList.remove('active');
+        this.classList.add('not-active');
+        document.querySelector('.nav_menu').classList.remove('active');
+        document.querySelector('.nav_menu').classList.add('not-active');
+
+    }
+    else {
+        this.classList.add('active');
+        this.classList.remove('not-active');
+        document.querySelector('.nav_menu').classList.remove('not-active');
+        document.querySelector('.nav_menu').classList.add('active');
+    }
+})
