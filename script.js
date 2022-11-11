92f04679787f34f43db344e7d726d703a03f3544
MarKraken <110046283+mariasmoUN@users.noreply.github.com>
2022-11-11 18:00:43 -0500

    Botón de ir arriba

--- script.js
+++ script.js
@@ -21,3 +21,20 @@ btn.addEventListener('click', function() {
         document.querySelector('.nav_menu').classList.add('active');
     }
 })
+
+/* Boton y función ir arriba */
+window.onscroll = function() {
+    if (document.documentElement.scrollTop > 100) {
+        document.querySelector('.go-top-container').classList.add('show');
+    }
+    else {
+        document.querySelector('.go-top-container').classList.remove('show');
+    }
+}
+
+document.querySelector('.go-top-container').addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
